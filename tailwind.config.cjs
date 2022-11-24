b7866149c8466af2fb9d0afc5c8ae7d24113f1b7
Ben Borgers <borgersbenjamin@gmail.com>
2022-11-23 22:33:23 -0500

    Write first explanation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
@@ -5,6 +6,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Switzer", ...defaultTheme.fontFamily.sans],
+      },
       colors: {
         gray: colors.zinc,
       },
